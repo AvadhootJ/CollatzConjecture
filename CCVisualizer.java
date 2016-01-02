93b1fa7013b62e07a3ae8c5bfcf3d8685ca255ef
Avadhoot Joshi <avadhootjoshi55@gmail.com>
2016-01-01 22:32:14 -0800

    This is the first version of the Visualizer

---
+++ CCVisualizer.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+/**
+ * Created by Avadhoot Joshi.
+ * The Collatz Conjecture is an interesting mathematical problem.
+ * This program helps us to visualize how a number reaches 1 using Collatz Conjecture.
+ * Basic rules to know, if number is even, n/2, if odd, 3n+1.
+ * Please do not enter zero.
+ */
+public class CCVisualizer {
+
+    public static void main(String[] args) {
+        int counter = 0; // Counts number of calculations
+        String calculations = ""; // Displays the number's path to 1.
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int number = s.nextInt();
+
+
+        if (number == 1) {
+            calculations += number + " " + "--->" + " ";
+            counter = 0;
+        }
+
+
+        while (number != 1) {
+
+            if (number % 2 == 0) {
+                number = number / 2;
+                counter++;
+                calculations += number + " " + "--->" + " ";
+
+            } else {
+                number = (3 * number) + 1;
+                counter++;
+                calculations += number + " " + "--->" + " ";
+
+            }
+
+
+        }
+        System.out.println(calculations.substring(0, calculations.length() - 5));
+        System.out.println("It took " + counter + " calculation(s) to reach the number 1 ");
+    }
+}
+
+
